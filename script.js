346d473c34be8badf79448905b2cf2c17b56b6cd
Enofua Divine <divineenofua@gmail.com>
2022-09-09 07:55:49 -1200

    last commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const currency_one = document.getElementById('currency-one');
-const amount_one = document.getElementById('currency-one');
+const amount_one = document.getElementById('amount-one');
 const currency_two = document.getElementById('currency-two');
-const amount_two = document.getElementById('currency-two');
+const amount_two = document.getElementById('amount-two');
 
-const rate = document.getElementById('rate');
+const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
 // Fetch exchange rate & update the Dom
@@ -13,11 +13,16 @@ function calculate() {
     const currencyEl_one = currency_one.value;
     const currencyEL_two = currency_two.value;
 
-    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyEl_one}`)
+    fetch(`https://v6.exchangerate-api.com/v6/3a981741a1915783946ae990/latest/${currencyEl_one}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-    });
+       
+        const rate = data.conversion_rates[currencyEL_two];
+    
+        rateEl.innerText = `1 ${currencyEl_one} = ${rate} ${currencyEL_two}`;
+        amount_two.value = (amount_one.value * rate).toFixed(2);
+
+     });
 }
    
 //Event Listeners
@@ -25,6 +30,12 @@ currency_one.addEventListener('change', calculate);
 amount_one.addEventListener('input', calculate);
 currency_two.addEventListener('change', calculate);
 amount_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+    const temp =currency_one.value;
+    currency_one.value = currency_two.value;
+    currency_two.value = temp;
+    calculate();
+});
 
 
 
